a157de89e30c7b4a392da64f332f40831e07b107
Allen Peng <boonya@sina.com>
2017-05-10 09:29:11 +0800

    Add some notes.

--- dubbo-zookeeper-spring-customer/src/main/java/com/boonya/zookeeper/SyncPrimitive.java
+++ dubbo-zookeeper-spring-customer/src/main/java/com/boonya/zookeeper/SyncPrimitive.java
@@ -1,11 +1,18 @@
 package com.boonya.zookeeper;
 
 import java.io.IOException;
-
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
-
+/**
+ * 原始同步初始化Zookeeper客户端连接和监听
+ * 
+ * @package com.boonya.zookeeper.SyncPrimitive
+ * @date   2017年5月10日  上午9:26:09
+ * @author pengjunlin
+ * @comment   
+ * @update
+ */
 public class SyncPrimitive implements Watcher {
 
     static ZooKeeper zk = null;
@@ -14,6 +21,10 @@ public class SyncPrimitive implements Watcher {
 
     String root;
 
+    /**
+     * 初始化Zookeeper连接
+     * @param address
+     */
     SyncPrimitive(String address) {
         if(zk == null){
             try {
@@ -29,9 +40,12 @@ public class SyncPrimitive implements Watcher {
         //else mutex = new Integer(-1);
     }
 
+    /**
+     * 监听观察者同步处理
+     */
     synchronized public void process(WatchedEvent event) {
         synchronized (mutex) {
-            //System.out.println("Process: " + event.getType());
+            System.out.println("Process: " + event.getType());
             mutex.notify();
         }
     }
