34cf6592ddd6b94024da4c0827d0535859a3d96b
Allen Peng <boonya@sina.com>
2017-05-10 09:17:44 +0800

    Edit sys properties.

--- dubbo-zookeeper-spring-customer/src/test/java/com/boonya/zookeeper/config/SysConfigTest.java
+++ dubbo-zookeeper-spring-customer/src/test/java/com/boonya/zookeeper/config/SysConfigTest.java
@@ -2,7 +2,15 @@ package com.boonya.zookeeper.config;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+/**
+ * 测试系统配置服务
+ * 
+ * @package com.boonya.zookeeper.config.SysConfigTest
+ * @date   2017年5月9日  下午5:20:28
+ * @author pengjunlin
+ * @comment   
+ * @update
+ */
 public class SysConfigTest {
 	
   private final static Logger logger = LoggerFactory
@@ -11,26 +19,26 @@ public class SysConfigTest {
   @Test
    public void testConfig(){
 	   SysConfig root = new SysConfigImpl("192.168.200.122:2181");
-       root.add("testKey1", "1");  
-       root.add("testKey2", "2");  
-       root.add("testKey3", "3");  
-       root.add("testKey4", "4");  
-       root.add("testKey5", "5");  
-       root.add("testKey6", "6");  
-       logger.info("value is===>"+root.get("testKey1"));  
-       logger.info("value is===>"+root.get("testKey2"));  
-       logger.info("value is===>"+root.get("testKey3"));  
-       logger.info("value is===>"+root.get("testKey4"));  
-       logger.info("value is===>"+root.get("testKey5"));  
-       logger.info("value is===>"+root.get("testKey6"));  
-       root.update("testKey6", "testKey6");  
-       logger.info("update testKey6 value is===>"+root.get("testKey6"));  
-       root.delete("testKey1");  
-       root.delete("testKey2");  
-       root.delete("testKey3");  
-       root.delete("testKey4");  
-       root.delete("testKey5");  
-       root.delete("testKey6");  
+       root.add("api_service_property1", "1");  
+       root.add("api_service_property2", "2");  
+       root.add("api_service_property3", "3");  
+       root.add("api_service_property4", "4");  
+       root.add("api_service_property5", "5");  
+       root.add("api_service_property6", "6");  
+       logger.info("value is===>"+root.get("api_service_property1"));  
+       logger.info("value is===>"+root.get("api_service_property2"));  
+       logger.info("value is===>"+root.get("api_service_property3"));  
+       logger.info("value is===>"+root.get("api_service_property4"));  
+       logger.info("value is===>"+root.get("api_service_property5"));  
+       logger.info("value is===>"+root.get("api_service_property6"));  
+       root.update("api_service_property6", "api_service_property6");  
+       logger.info("update api_service_property6 value is===>"+root.get("api_service_property6"));  
+       root.delete("api_service_property1");  
+       root.delete("api_service_property2");  
+       root.delete("api_service_property3");  
+       root.delete("api_service_property4");  
+       root.delete("api_service_property5");  
+       root.delete("api_service_property6");  
    }
 
 }
